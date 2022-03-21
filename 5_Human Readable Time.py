dcfe146eb1752bd6bac3449932ad2c4483a0234c
Ejiks <ivanbirulia@gmail.com>
2022-03-21 13:09:58 +0300

    5_Human Readable Time Done

--- 5_Human Readable Time.py
+++ 5_Human Readable Time.py
@@ -1,16 +1,19 @@
 def make_readable(seconds):
-    hh = seconds//(60**2)
-    if len(str(hh)) != 2:
-        hh = f"0{hh}"
-    mm = seconds//(60)-hh*60
-    if len(str(mm)) != 2:
-        mm = f"0{mm}"
-    ss = seconds - mm*60 - hh*60*60
-    if len(str(ss)) != 2:
-        ss = f"0{ss}"
+    ss = 0
+    mm = 0
+    hh = 0
+    for i in range(seconds):
+        if ss <59:
+            ss +=1
+        else:
+            if mm <59:
+                mm +=1
+                ss =0
+            else:
+                hh +=1
+                mm =0
+                ss =0
+    return f'{hh:02}:{mm:02}:{ss:02}'
 
-    
-    return f'{hh}:{mm}:{ss}'
 
-
-print(make_readable(60))
+print(make_readable(359999))
